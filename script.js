cdcd065b5712e45b42363b6ab4bf64bfa17c7bab
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-02-16 16:05:25 +0300

    The first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let countries = ["Kenya", "Uganda", "Tanzania", "Rwanda", "Burudi"];
+countries.map((country) => console.log(country));
+
+let students = [
+  {
+    name: "Jose",
+    regNo: "sct211-0011/2018",
+    course: "Computer Science",
+    school: "JKUAT",
+  },
+  {
+    name: "Juma",
+    regNo: "sct211-0001/2018",
+    course: "Computer Science",
+    school: "JKUAT",
+  },
+  {
+    name: "Manu",
+    regNo: "sct211-0020/2018",
+    course: "Computer Science",
+    school: "KU",
+  },
+];
+students.map((student) => {
+  return console.log(`
+  Student Detail: 
+   Name : ${student.name}
+   Registration Number : ${student.regNo}
+   Course  : ${student.course}
+   School : ${student.school}
+  `);
+});
